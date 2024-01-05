96369d7875fed29876d8e6e5117928b8086e3880
Hezekiah Elisha <elishahezekiah903@gmail.com>
2024-01-06 02:12:39 +0300

    :construction: Add API initial

--- api/app.js
+++ api/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
